794322d7203d081a97092d16598c976a40082d26
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-10 23:11:51 -0500

    feat: create instance of dynamodb with aws

--- src/addTask.js
+++ src/addTask.js
@@ -1,6 +1,9 @@
 const { v4 } = require('uuid');
+const AWS = require('aws-sdk');
 
 const addTask = async event => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
   const { title, description } = event.body;
   const createdAt = new Date();
   const id = v4();
