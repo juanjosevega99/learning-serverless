3e9785934989a992406902dbdcabf729991dd862
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-10 23:09:46 -0500

    feat: add the id with v4 uuid

--- src/addTask.js
+++ src/addTask.js
@@ -1,6 +1,9 @@
+const { v4 } = require('uuid');
+
 const addTask = async event => {
   const { title, description } = event.body;
   const createdAt = new Date();
+  const id = v4();
 };
 
 module.exports = {
