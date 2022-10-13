25d26beae7e7a32a9789cebbc4e891ca6da9cb48
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-12 22:29:10 -0500

    feat: get tasks

---
+++ src/getTasks.js
@@ -0,0 +1,15 @@
+const AWS = require('aws-sdk');
+
+const getTasks = async () => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const result = await dynamoDb.scan({ TableName: 'TaskTable' }).promise();
+
+  const tasks = result.Items;
+
+  return { status: 200, body: { tasks } };
+};
+
+module.exports = {
+  getTasks
+};
