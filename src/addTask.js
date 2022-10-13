ef973b4a287354253b53e2ce6f860a4175594a6a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-12 22:28:43 -0500

    feat: add task

--- src/addTask.js
+++ src/addTask.js
@@ -4,9 +4,20 @@ const AWS = require('aws-sdk');
 const addTask = async event => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  const { title, description } = event.body;
+  const { title, description } = JSON.parse(event.body);
   const createdAt = new Date();
   const id = v4();
+
+  const newTask = { id, title, description, createdAt };
+
+  await dynamoDb
+    .put({
+      TableName: 'taskTable',
+      Item: newTask
+    })
+    .promise();
+
+  return { status: 200, body: JSON.stringify(newTask) };
 };
 
 module.exports = {
