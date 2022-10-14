9a5905053c1816ed010187e73ac8c1d6a28a6f4e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-13 22:48:05 -0500

    fix: table name

--- src/getTasks.js
+++ src/getTasks.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 const getTasks = async () => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  const result = await dynamoDb.scan({ TableName: 'TaskTable' }).promise();
+  const result = await dynamoDb.scan({ TableName: 'taskTable' }).promise();
 
   const tasks = result.Items;
 
