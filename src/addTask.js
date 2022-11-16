1b5336a647231c0cd0c65915d44af76b742b4060
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-11-16 18:51:49 -0500

    refactor: use middy to parse to json

--- src/addTask.js
+++ src/addTask.js
@@ -1,10 +1,12 @@
 const { v4 } = require('uuid');
 const AWS = require('aws-sdk');
+const middy = require('@middy/core');
+const jsonBodyParser = require('@middy/http-json-body-parser');
 
 const addTask = async event => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  const { title, description } = JSON.parse(event.body);
+  const { title, description } = event.body;
   const createdAt = new Date();
   const id = v4();
 
@@ -21,5 +23,5 @@ const addTask = async event => {
 };
 
 module.exports = {
-  addTask
+  addTask: middy(addTask).use(jsonBodyParser)
 };
